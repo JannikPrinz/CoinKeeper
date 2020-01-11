8f0e80afc48427da149553398020d40facdb1fed
Jannik Prinz <jannik.prinz@gmail.com>
2020-01-11 17:28:25 +0100

    [Refactoring] Refactor LabelManager. Fix Issue #10

--- CoinKeeper/labelmanager.cpp
+++ CoinKeeper/labelmanager.cpp
@@ -1,5 +1,9 @@
 #include "labelmanager.h"
 
+#include "qcolordialog.h"
+#include "qinputdialog.h"
+#include "qmessagebox.h"
+
 LabelManager::LabelManager(std::shared_ptr<Database> data) :
     database(data)
 {
@@ -7,98 +11,83 @@ LabelManager::LabelManager(std::shared_ptr<Database> data) :
 
 void LabelManager::ManageLabels()
 {
-    manageLabelsWindow = new Ui::ManageLabelsWindow;
+    auto manageLabelsWindow = std::make_unique<Ui::ManageLabelsWindow>();
     QDialog dialog;
     manageLabelsWindow->setupUi(&dialog);
-    RefreshWindow();
-    connect(manageLabelsWindow->buttonNewLabel, &QPushButton::clicked, this, [=] { CreateNewLabel(); });
-    connect(manageLabelsWindow->buttonChangeName, &QPushButton::clicked, this, [=] { ChangeLabelName(); });
-    connect(manageLabelsWindow->buttonChangeColor, &QPushButton::clicked, this, [=] { ChangeLabelColor(); });
-    connect(manageLabelsWindow->buttonDeleteLabel, &QPushButton::clicked, this, [=] { DeleteLabel(); });
+    RefreshWindow(manageLabelsWindow);
+
+    connect(manageLabelsWindow->buttonNewLabel, &QPushButton::clicked, this, [this, &manageLabelsWindow] {
+        CreateNewLabel();
+        RefreshWindow(manageLabelsWindow);
+    });
+    connect(manageLabelsWindow->buttonChangeName, &QPushButton::clicked, this, [this, &manageLabelsWindow] {
+        ChangeLabelName(manageLabelsWindow->tableLabels->currentRow());
+        RefreshWindow(manageLabelsWindow);
+    });
+    connect(manageLabelsWindow->buttonChangeColor, &QPushButton::clicked, this, [this, &manageLabelsWindow] {
+        ChangeLabelColor(manageLabelsWindow->tableLabels->currentRow());
+        RefreshWindow(manageLabelsWindow);
+    });
+    connect(manageLabelsWindow->buttonDeleteLabel, &QPushButton::clicked, this, [this, &manageLabelsWindow] {
+        DeleteLabel(manageLabelsWindow->tableLabels->currentRow());
+        RefreshWindow(manageLabelsWindow);
+    });
+
     dialog.exec();
-    delete manageLabelsWindow;
-    manageLabelsWindow = nullptr;
 }
 
 void LabelManager::CreateNewLabel()
 {
     bool userClickedOk;
     QString text = QInputDialog::getText(Q_NULLPTR, QString::fromStdString(TEXT_CREATE_NEW_LABEL), QString::fromStdString(TEXT_NAME_COLON), QLineEdit::Normal, "", &userClickedOk);
-    if (userClickedOk && !text.isEmpty())
-    {
+    if (userClickedOk && !text.isEmpty()) {
         QColor color = QColorDialog::getColor(Qt::white, Q_NULLPTR, QString::fromStdString(TEXT_CHOOSE_COLOR), QColorDialog::ShowAlphaChannel);
-        if (color.isValid())
-        {
+        if (color.isValid()) {
             database->CreateNewLabel(text.toStdString(), ConvertRGBAToInt(color.red(), color.green(), color.blue(), color.alpha()));
-            RefreshWindow();
-            //qDebug(std::to_string(color.value()).c_str());    // Helligkeit
-            //int dr, dg, db, da;
-            //tie(dr, dg, db, da) = ConvertIntToRGBA(-1);
-            //string sd = "/nDecoded:";
-            //sd.append(std::to_string(dr));
-            //sd.append(", ");
-            //sd.append(std::to_string(dg));
-            //sd.append(", ");
-            //sd.append(std::to_string(db));
-            //sd.append(", ");
-            //sd.append(std::to_string(da));
-            //qDebug(sd.c_str());
         }
     }
 }
 
-void LabelManager::ChangeLabelName()
+void LabelManager::ChangeLabelName(int const selectedRow)
 {
-    int selectedRow = manageLabelsWindow->tableLabels->currentRow();
-    if (selectedRow >= 0)
-    {
+    if (selectedRow >= 0) {
         std::string oldText;
         int labelID, color;
         tie(labelID, oldText, color) = currentLabels[selectedRow];
         bool userClickedOk;
         QString text = QInputDialog::getText(Q_NULLPTR, QString::fromStdString(TEXT_CHANGE_LABEL), QString::fromStdString(TEXT_NAME_COLON), QLineEdit::Normal, QString::fromStdString(oldText), &userClickedOk);
-        if (userClickedOk && !text.isEmpty())
-        {
+        if (userClickedOk && !text.isEmpty()) {
             database->UpdateLabel(labelID, text.toStdString(), color);
-            RefreshWindow();
         }
     }
 }
 
-void LabelManager::ChangeLabelColor()
+void LabelManager::ChangeLabelColor(int const selectedRow)
 {
-    int selectedRow = manageLabelsWindow->tableLabels->currentRow();
-    if (selectedRow >= 0)
-    {
+    if (selectedRow >= 0) {
         std::string text;
         int labelID, oldColor;
         tie(labelID, text, oldColor) = currentLabels[selectedRow];
         QColor color = QColorDialog::getColor(ConvertIntToQColor(oldColor), Q_NULLPTR, QString::fromStdString(TEXT_CHOOSE_COLOR), QColorDialog::ShowAlphaChannel);
-        if (color.isValid())
-        {
+        if (color.isValid()) {
             database->UpdateLabel(labelID, text, ConvertRGBAToInt(color.red(), color.green(), color.blue(), color.alpha()));
-            RefreshWindow();
         }
     }
 }
 
-void LabelManager::DeleteLabel()
+void LabelManager::DeleteLabel(int const selectedRow)
 {
-    int selectedRow = manageLabelsWindow->tableLabels->currentRow();
-    if (selectedRow >= 0)
-    {
+    if (selectedRow >= 0) {
         int labelID = std::get<0>(currentLabels[selectedRow]);
         QMessageBox msg;
-        if (labelID == 1)    // default label selected
-        {
+        if (labelID == 1) {   // default label selected
             msg.setText(QString::fromStdString(TEXT_DEFAULT_LABEL_NOT_DELETABLE));
             msg.exec();
             return;
         }
         msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
         msg.setText(QString::fromStdString(TEXT_QUESTION_DELETE_LABEL));
-        switch (msg.exec())
-        {
+        switch (msg.exec()) {
         case QMessageBox::Yes:
             database->DeleteLabel(labelID);
             break;
@@ -107,33 +96,26 @@ void LabelManager::DeleteLabel()
         default:
             break;
         }
-        RefreshWindow();
     }
 }
 
-void LabelManager::RefreshWindow()
+void LabelManager::RefreshWindow(std::unique_ptr<Ui::ManageLabelsWindow>& manageLabelsWindow)
 {
     // if the manage labels window is open, refresh the label list:
-    if (manageLabelsWindow != nullptr)
-    {
+    if (manageLabelsWindow != nullptr) {
         currentLabels = database->GetLabels();
         int x = static_cast<int>(currentLabels.size());
         manageLabelsWindow->tableLabels->setRowCount(x);
-        for (int i = 0; i < x; ++i)
-        {
+        for (int i = 0; i < x; ++i) {
             int labelID;        // not used
             std::string labelName;
             int color;
             tie(labelID, labelName, color) = currentLabels[i];
             QColor qColor = ConvertIntToQColor(color);
             manageLabelsWindow->tableLabels->setItem(i, 0, new QTableWidgetItem(QString::fromStdString(labelName)));
-            manageLabelsWindow->tableLabels->setItem(i, 1, new QTableWidgetItem(QString::number(color)));
+            manageLabelsWindow->tableLabels->setItem(i, 1, new QTableWidgetItem(QString()));
             manageLabelsWindow->tableLabels->item(i, 1)->setBackgroundColor(qColor);
             manageLabelsWindow->tableLabels->item(i, 1)->setTextColor((qColor.value() + (255 - qColor.alpha()) < 128) ? Qt::white : Qt::black);
         }
     }
-}
-
-LabelManager::~LabelManager()
-{
 }
--- CoinKeeper/labelmanager.h
+++ CoinKeeper/labelmanager.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include "ui_managelabels.h"
 #include "database.h"
-#include "qinputdialog.h"
-#include "qcolordialog.h"
-#include "qmessagebox.h"
+#include "ui_managelabels.h"
 
 class LabelManager : QObject
 {
@@ -15,31 +12,39 @@ public:
      * Creates a new LabelManager with the given parameters.
      *
      * Parameters:
-     * string profilePath : path of the profile
-     * Database* data : adress of a database object
+     * std::shared_ptr<Database> data : the active database
      */
     LabelManager(std::shared_ptr<Database> data);
-    // Opens a new Dialog-Window to manage the labels. After execution is the window closed.
+    ~LabelManager() = default;
+    /*
+     * Opens a new Dialog-Window to manage the labels. This window is closed, before this method returns.
+     */
     void ManageLabels();
-    ~LabelManager();
 
 private:
-    Ui::ManageLabelsWindow* manageLabelsWindow = nullptr;
-    std::shared_ptr<Database> database;
-    // all labels, which are display in the manage labels window (if open)
-    std::vector<std::tuple<int, std::string, int>> currentLabels;
-    // Creates a new label using inputs of the user.
+    /*
+     * Creates a new label using inputs of the user.
+     */
     void CreateNewLabel();
     /*
      * Changes the name of a labal using inputs of the user.
      */
-    void ChangeLabelName();
-    // Changes the color of a label using inputs of the user.
-    void ChangeLabelColor();
+    void ChangeLabelName(int selectedRow);
+    /*
+     * Changes the color of a label using inputs of the user.
+     */
+    void ChangeLabelColor(int selectedRow);
     /*
      * Deletes the label, if it is not the default label (= label with the smallest id).
      */
-    void DeleteLabel();
-    // Refreshs the window, which shows all labels.
-    void RefreshWindow();
+    void DeleteLabel(int selectedRow);
+    /*
+     * Refreshs the window, which shows all labels.
+     */
+    void RefreshWindow(std::unique_ptr<Ui::ManageLabelsWindow>& window);
+
+private:
+    std::shared_ptr<Database> database;
+    // all labels, which are display in the manage labels window (if open)
+    std::vector<std::tuple<int, std::string, int>> currentLabels;
 };
